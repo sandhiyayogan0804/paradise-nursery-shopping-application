919782d417e3da009f909ba1970c0e27a5e2d2ce
sandhiyayogan0804 <sandhiyayogan0804@gmail.com>
2026-08-13 09:35:21 +0530

    Create AboutUs component for nursery information
    Added AboutUs component to display information about Paradise Nursery.

---
+++ aboutUS.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your destination for beautiful
+        houseplants.
+      </p>
+
+      <p>
+        We offer a wide variety of plants to help make your home greener,
+        fresher, and more beautiful.
+      </p>
+
+      <p>
+        Our goal is to provide quality plants and make plant shopping simple
+        and enjoyable.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
